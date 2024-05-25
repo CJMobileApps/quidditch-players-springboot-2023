973596ed60a92e23fa8f76e876a7e3c29b78509c
Chris Jones <chris@cjmobileapps.com>
2024-05-25 00:01:07 -0500

    Finish house test

---
+++ src/test/kotlin/com/cjmobileapps/quidditchplayers/api/house/repository/HouseRepositoryTest.kt
@@ -0,0 +1,30 @@
+package com.cjmobileapps.quidditchplayers.api.house.repository
+
+import com.cjmobileapps.quidditchplayers.data.MockData
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class HouseRepositoryTest {
+
+    private lateinit var houseRepository: HouseRepository
+
+    @BeforeEach
+    private fun setup() {
+        houseRepository = HouseRepository()
+    }
+
+    @Test
+    fun `getAllHouses success call`(): Unit = runBlocking {
+
+        // then
+        val houses = houseRepository.getAllHouses()
+
+        // verify
+        Assertions.assertEquals(
+            MockData.houses,
+            houses
+        )
+    }
+}
---
+++ src/test/kotlin/com/cjmobileapps/quidditchplayers/api/house/service/HouseServiceTest.kt
@@ -0,0 +1,41 @@
+package com.cjmobileapps.quidditchplayers.api.house.service
+
+import com.cjmobileapps.quidditchplayers.api.house.repository.HouseDao
+import com.cjmobileapps.quidditchplayers.data.MockData
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class HouseServiceTest {
+
+    @Mock
+    lateinit var mockHouseDao: HouseDao
+
+    lateinit var houseService: HouseService
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        houseService = HouseServiceImpl(mockHouseDao)
+    }
+
+    @Test
+    fun `getAllHouses success call`(): Unit = runBlocking {
+
+        // when
+        Mockito.`when`(mockHouseDao.getAllHouses()).thenReturn(MockData.houses)
+
+        // then
+        val houses = MockData.houses
+
+        // verify
+        Assertions.assertEquals(
+            MockData.houses,
+            houses
+        )
+    }
+}
