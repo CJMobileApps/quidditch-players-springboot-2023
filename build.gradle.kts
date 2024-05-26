7351eaee01231868bf22a8a002cbf7904bd68ad6
Chris Jones <chris@cjmobileapps.com>
2024-05-26 13:28:38 -0500

    Add jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.20"
 	kotlin("plugin.spring") version "1.9.20"
+	jacoco
 }
 
 group = "com.cjmobileapps"
@@ -48,4 +49,11 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
 	jvmTarget = JavaVersion.VERSION_17.majorVersion
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
 }
